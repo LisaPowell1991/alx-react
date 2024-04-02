8838cc25045f2f50c3c86fb8c785d972dab9a385
Lisa Powell <lisapowell46@gmail.com>
2024-04-02 15:52:11 +0200

    updated files

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -10,8 +10,12 @@ $('body').append('<p>Copyright - Holberton School</p>');
 let count = 0;
 
 function updateCounter() {
-    count++;
-    $("#count").html(`${count} clicks on the button`);
-};
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+}
 
-$('button').on('click', _.debounce(updateCounter, 500));
+// Debounce to prevent spamming
+const debouncedUpdateCounter = _.debounce(updateCounter, 300);
+
+// Bind the debounced function to button click event
+$('#start-btn').on('click', debouncedUpdateCounter);
