d92c9d9fbf8606091e1eb1a42741937dc21f4b8c
Lisa Powell <lisapowell46@gmail.com>
2024-04-02 15:58:22 +0200

    updated files

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,21 +1,15 @@
 import $ from 'jquery';
 import _ from 'lodash';
-
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
-
 let count = 0;
 
 function updateCounter() {
-    count += 1;
-    $('#count').text(`${count} clicks on the button`);
-}
+    count++;
+    $("#count").html(`${count} clicks on the button`);
+};
 
-// Debounce to prevent spamming
-const debouncedUpdateCounter = _.debounce(updateCounter, 300);
-
-// Bind the debounced function to button click event
-$('#start-btn').on('click', debouncedUpdateCounter);
+$('button').on('click', _.debounce(updateCounter, 500));
