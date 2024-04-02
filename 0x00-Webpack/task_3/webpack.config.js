f73c8b1a1cd8aaf7ec3ff4f38630debad79bcae4
Lisa Powell <lisapowell46@gmail.com>
2024-04-02 23:03:25 +0200

    busy with Dev servers, modules, and tree shaking

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: './index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    devtool: 'inline-source-map',
+    mode: 'development',
+    entry: {
+        header: {
+            import: './modules/header/header.js',
+            dependOn: 'shared',
+        },
+        body: {
+            import: './modules/body/body.js',
+            dependOn: 'shared',
+        },
+        footer: {
+            import: './modules/footer/footer.js',
+            dependOn: 'shared',
+        },
+        shared: 'jquery',
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        port: 8564,
+        open: true,
+        hot: true,
+        liveReload: true,
+    },
+    performance: {
+        hints: false,
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/[name][hash][ext]',
+                },
+            },
+        ],
+    },
+};
