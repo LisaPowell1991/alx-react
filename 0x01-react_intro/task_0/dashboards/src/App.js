e08a85d927e82fa0fec79dbb3cb5caf59251fff5
Lisa Powell <lisapowell46@gmail.com>
2024-04-03 14:39:20 +0200

    changed dashboard to dashboards

--- 0x01-react_intro/task_0/dashboards/src/App.js
+++ 0x01-react_intro/task_0/dashboards/src/App.js
@@ -8,12 +8,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </div>
-
-      <div className="App-body">
+      <div className='App-body'>
         <p>Login to access the full dashboard</p>
       </div>
-
-      <div className="App-footer">
+      <div className='App-footer'>
         <p>Copyright 2020 - holberton School</p>
       </div>
     </div>
