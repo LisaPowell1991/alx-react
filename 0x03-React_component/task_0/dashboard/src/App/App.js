fc943540afa911d92d2c0b68a700843a837ca880
Lisa Powell <lisapowell46@gmail.com>
2024-04-10 11:48:27 +0200

    Convert the App function into a React Class

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notification from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCourses: [
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 }
+      ],
+
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
+      ]
+    };
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    const { listCourses, listNotifications } = this.state;
+
+    const updatedListNotifications = listNotifications.map((notification) => ({
+      ...notification,
+      id: notification.id + listNotifications.length
+    }));
+
+    return (
+      <React.Fragment>
+        <Notification listNotifications={updatedListNotifications} />
+        <div className="App">
+          <Header />
+          <div className="App-body">
+            {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          </div>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+export default App;
