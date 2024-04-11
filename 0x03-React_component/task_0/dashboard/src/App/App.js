56a26c105b690e7ee7f69114fea8911b47574169
Lisa Powell <lisapowell46@gmail.com>
2024-04-11 10:10:13 +0200

    changed function to class

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import Header from '../Header/Header';
@@ -7,40 +7,27 @@ import Footer from '../Footer/Footer';
 import Notification from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
 
-class App extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = (event) => {
-    if (event.ctrlKey && event.key === 'h') {
-      alert('Logging you out');
-      this.props.logOut();
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCourses: [
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 }
+      ],
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
+      ]
+    };
   }
 
   render() {
-    const { isLoggedIn } = this.props;
-
-    const listCourses = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 }
-    ];
-
-    const listNotifications = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
-    ];
-
-    const updatedListNotifications = listNotifications.map((notification) => ({
+    const updatedListNotifications = this.state.listNotifications.map((notification) => ({
       ...notification,
-      id: notification.id + listNotifications.length
+      id: notification.id + this.state.listNotifications.length
     }));
 
     return (
@@ -49,7 +36,7 @@ class App extends Component {
         <div className="App">
           <Header />
           <div className="App-body">
-            {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+            {this.props.isLoggedIn ? <CourseList listCourses={this.state.listCourses} /> : <Login />}
           </div>
           <Footer />
         </div>
@@ -60,12 +47,10 @@ class App extends Component {
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func,
 };
 
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => { },
 };
 
 export default App;
--- 0x03-React_component/task_0/dashboard/src/CourseList/CourseList.test.js
+++ 0x03-React_component/task_0/dashboard/src/CourseList/CourseList.test.js
@@ -4,12 +4,38 @@ import CourseListRow from './CourseListRow';
 import CourseList from './CourseList';
 
 describe('<CourseList />', () => {
-    it('renders CourseList component without crashing', () => {
-        shallow(<CourseList />);
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<CourseList />);
     });
 
-    it('renders different rows', () => {
-        const wrapper = shallow(<CourseList />);
-        expect(wrapper.find(CourseListRow)).toHaveLength(3);
+    it('renders CourseList component without crashing', () => {
+        expect(wrapper).toBeTruthy();
+    });
+
+    describe('With CourseList Empty', () => {
+        beforeEach(() => {
+            wrapper.setProps({ listCourses: [] });
+        });
+
+        it('renders correctly if listCourses is not passed or is an empty array', () => {
+            expect(wrapper.find(CourseListRow)).toHaveLength(3);
+        });
+    });
+
+    describe('With CourseList containing elements', () => {
+        const courses = [
+            { id: 1, name: 'Course 1', credit: 60 },
+            { id: 2, name: 'Course 2', credit: 40 },
+        ];
+
+        beforeEach(() => {
+            wrapper.setProps({ listCourses: courses });
+        });
+
+        it('renders listCourses correctly when passed', () => {
+            expect(wrapper.find(CourseListRow)).toHaveLength(courses.length + 2);
+        });
     });
 });
