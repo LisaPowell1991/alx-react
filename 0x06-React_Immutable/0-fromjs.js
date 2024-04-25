7c2ac7dc89cfe876ff3ab7bb7e745b5a90cee0db
Lisa Powell <lisapowell46@gmail.com>
2024-04-25 11:47:17 +0200

    export default function

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,11 +1,6 @@
 import { fromJS } from 'immutable';
 
-function getImmutableObject(object) {
+export default function getImmutableObject(object) {
     // Convert the object to an Immutable object
     return fromJS(object);
 }
-
-/* // Test the function
-const testObject = { a: 1, b: 2, c: 3 };
-const immutableObject = getImmutableObject(testObject);
-console.log(immutableObject); */
