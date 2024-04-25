ed0d19dd40f2de88d78dc680efe223e24a9d0810
Lisa Powell <lisapowell46@gmail.com>
2024-04-25 11:38:49 +0200

    Converting into an Immutable object using fromJS

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,11 @@
+const { fromJS } = require('immutable');
+
+function getImmutableObject(object) {
+    // Convert the object to an Immutable object
+    return fromJS(object);
+}
+
+// Test the function
+const testObject = { a: 1, b: 2, c: 3 };
+const immutableObject = getImmutableObject(testObject);
+console.log(immutableObject);
