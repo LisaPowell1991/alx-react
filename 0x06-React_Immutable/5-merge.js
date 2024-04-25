12be5d7aeb34da6ee50f2ca7bad3e328d620746b
Lisa Powell <lisapowell46@gmail.com>
2024-04-25 16:45:36 +0200

    Merge & concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,11 +1,13 @@
 import { List, Map } from 'immutable';
 
+// Function to concatenate two arrays into an Immutable List
 export function concatElements(page1, page2) {
-    // Concatenate two lists to an immutable list
+    // Convert arrays to Lists and concatenate them
     return List(page1).concat(List(page2));
 }
 
+// Function to merge two objects into an Immutable List of their values, preferring page2's values
 export function mergeElements(page1, page2) {
-    // Merge 2 objects to an immutable object(Map)
+    // Convert objects to Maps, merge them, and then convert the values to a List
     return Map(page1).merge(Map(page2)).toList();
 }
