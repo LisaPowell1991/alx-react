b81f2080a1dcaff66affdd11ebe3dad553f81141
Lisa Powell <lisapowell46@gmail.com>
2024-04-25 17:03:29 +0200

    Nested merge

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
 // Function to deeply merge two objects into an Immutable Map
-export function mergeDeeplyElements(page1, page2) {
+export default function mergeDeeplyElements(page1, page2) {
     return Map(page1).mergeDeep(Map(page2));
 }
