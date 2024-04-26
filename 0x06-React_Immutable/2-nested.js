9133db0652c8555528072249c8e036f233650e17
lisapowell1991 <lisapowell46@gmail.com>
2024-04-26 09:45:14 +0000

    fixed eslint syntax error

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,9 +1,9 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-    // Convert obj to an Immutable obj
-    const immutableObj = fromJS(object);
+  // Convert obj to an Immutable obj
+  const immutableObj = fromJS(object);
 
-    // Access the value of the key in the array
-    return immutableObj.getIn(array, undefined);
+  // Access the value of the key in the array
+  return immutableObj.getIn(array, undefined);
 }
