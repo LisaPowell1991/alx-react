b12ced3916900721d3ffa8bc50b26c96290c3d22
lisapowell1991 <lisapowell46@gmail.com>
2024-04-26 09:53:37 +0000

    fixed eslint syntax error

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -2,10 +2,10 @@ import { List, Map } from 'immutable';
 
 // Function to concatenate two arrays into an Immutable List
 export function concatElements(page1, page2) {
-    return List(page1).concat(List(page2));
+  return List(page1).concat(List(page2));
 }
 
 // Function to merge two objects into an Immutable List of their values
 export function mergeElements(page1, page2) {
-    return Map(page1).merge(Map(page2));
+  return Map(page1).merge(Map(page2));
 }
