474b6b283c3f90001bdb0b2d2784aeda8dcd6ab0
lisapowell1991 <lisapowell46@gmail.com>
2024-04-26 09:51:12 +0000

    fixed eslint syntax error

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -2,16 +2,16 @@ import { Map } from 'immutable';
 
 // Create Immutable Map
 export const map = Map({
-    1: 'Liam',
-    2: 'Noah',
-    3: 'Elijah',
-    4: 'Oliver',
-    5: 'Jacob',
-    6: 'Lucas',
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
 });
 
 // Modify the value of the indices 2 and 4
 export const map2 = map.withMutations((values) => {
-    values.set(2, 'Benjamin')
-        .set(4, 'Oliver');
-});  
+  values.set(2, 'Benjamin')
+    .set(4, 'Oliver');
+});
