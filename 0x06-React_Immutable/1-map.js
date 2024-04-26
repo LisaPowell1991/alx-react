04835448d33ece65c0992200889790aecd0a5a7e
lisapowell1991 <lisapowell46@gmail.com>
2024-04-26 09:33:29 +0000

    fixed eslint syntax error

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,6 +1,7 @@
 import { Map } from 'immutable';
 
 export default function getImmutableObject(object) {
-    // convert the object to an Immutable object
-    return Map(object);
+  // convert the object to an Immutable object
+  return Map(object);
 }
+
