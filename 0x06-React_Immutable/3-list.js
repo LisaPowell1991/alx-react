520401b6f2e8b2c55836ee3decab9df68488bb5c
lisapowell1991 <lisapowell46@gmail.com>
2024-04-26 09:47:54 +0000

    fixed eslint syntax error

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,11 +1,11 @@
 import { List } from 'immutable';
 
 export function getListObject(array) {
-    // Convert the array to an Immutable List
-    return List(array);
+  // Convert the array to an Immutable List
+  return List(array);
 }
 
 export function addElementToList(list, element) {
-    // Takes immutable list and adds element to it
-    return list.push(element);
+  // Takes immutable list and adds element to it
+  return list.push(element);
 }
