7abeab06ba4ee7511692f49ec0fdd4c0a76394a3
Lisa Powell <lisapowell46@gmail.com>
2024-04-29 16:10:26 +0200

    Read data from a JSON

---
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -0,0 +1,5 @@
+import * as notificationData from "../../notifications.json";
+
+export function getAllNotificationsByUser(userId) {
+    return notificationData.default.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
+}
