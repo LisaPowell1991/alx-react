a801a2ec5e7cb0c5584bc1aa62839e16ce3a56f6
Lisa Powell <lisapowell46@gmail.com>
2024-05-15 16:34:29 +0200

    Connect user state to the Footer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,13 +1,10 @@
 // Footer.js
-import React, { useContext } from "react";
+import React from "react";
 import "./Footer.css";
 import { getFullYear, getFooterCopy } from "../utils/utils";
-import AppContext from "../App/AppContext";
+import { connect } from 'react-redux';
 
-function Footer({ user: testUser }) {
-	const contextUser = useContext(AppContext);
-	const user = testUser || contextUser.user;
-
+function Footer({ user }) {
 	return (
 		<>
 			<div className="App-footer">
@@ -24,4 +21,10 @@ function Footer({ user: testUser }) {
 	);
 }
 
-export default Footer;
+const mapStateToProps = state => {
+	return {
+		user: state.user
+	};
+};
+
+export default connect(mapStateToProps)(Footer);
