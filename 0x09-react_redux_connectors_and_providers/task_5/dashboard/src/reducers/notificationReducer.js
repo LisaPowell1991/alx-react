5f0e93801d5a57af0e430938c787d702858b90e1
Lisa Powell <lisapowell46@gmail.com>
2024-05-17 16:58:19 +0200

    Connect notifications: Improve reducer

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
@@ -3,31 +3,38 @@ import {
     FETCH_NOTIFICATIONS_SUCCESS,
     MARK_AS_READ,
     SET_TYPE_FILTER,
+    SET_LOADING_STATE,
 } from '../actions/notificationActionTypes';
 import { notificationsNormalizer } from '../schema/notifications';
 
 export const initialNotificationState = Map({
-    notifications: [],
+    notifications: Map({
+        entities: Map(),
+        result: [],
+    }),
     filter: 'DEFAULT',
+    loading: false,
 });
 
 const notificationReducer = (state = initialNotificationState, action) => {
     switch (action.type) {
         case FETCH_NOTIFICATIONS_SUCCESS:
-            return state.merge({
-                notifications: notificationsNormalizer(action.data),
+            return state.mergeDeep({
+                notifications: notificationsNormalizer(action.notifications),
             });
 
         case MARK_AS_READ:
-            return state.setIn(['notifications', action.index, 'isRead'], true);
+            return state.setIn(['notifications', 'entities', action.index, 'isRead'], true);
 
         case SET_TYPE_FILTER:
             return state.set('filter', action.filter);
 
+        case SET_LOADING_STATE:
+            return state.set('loading', action.isLoading);
+
         default:
             return state;
     }
-
 };
 
 export default notificationReducer;
